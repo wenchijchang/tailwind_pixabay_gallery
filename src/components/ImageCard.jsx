6b61efcb32b7911488817b0ff260b8f5fea29c4f
wenchijchang <heyyhey@hotmail.com>
2023-08-08 09:21:49 -0400

    Changed font color in ImageCard

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -7,7 +7,7 @@ const ImageCard = ({ image }) => {
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src={image.webformatURL} alt="" className="w-full" />
       <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
+        <div className="font-bold text-teal-500 text-xl mb-2">
           Photo by {image.user}
         </div>
         <ul>
